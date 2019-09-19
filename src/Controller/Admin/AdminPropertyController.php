749613596e8e6232a1cba3bd0b4a38df4151ecfe
Alexandre <contact@steptosuccess.fr>
2019-09-19 11:10:15 +0200

    CRUD des biens

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -36,7 +37,29 @@ class AdminPropertyController extends AbstractController{
     }
 
     /**
-     * @Route("/admin/{id}", name="admin.property.edit")
+     * @Route("/admin/create", name="admin.property.new")
+     */
+    public function new(Request $request){
+
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($property);
+            $this->manager->flush();
+            $this->addFlash("success", "Créé avec succès");
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+        return $this->render("admin/new.html.twig", [
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/{id}", name="admin.property.edit", methods="GET|POST")
      * @param Property $property
      */
     public function edit(Property $property, Request $request)
@@ -46,6 +69,7 @@ class AdminPropertyController extends AbstractController{
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->manager->flush();
+            $this->addFlash("success", "Modifié avec succès");
             return $this->redirectToRoute('admin.property.index');
         }
 
@@ -53,6 +77,21 @@ class AdminPropertyController extends AbstractController{
             'property' => $property,
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/admin/{id}", name="admin.property.delete", methods="DELETE")
+     * @param Property $property
+     */
+    public function delete(Property $property, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete'. $property->getId(), $request->get('_token'))){
+
+            $this->manager->remove($property);
+            $this->manager->flush();
+            $this->addFlash("success", "Supprimé avec succès");
+        }
+        return $this->redirectToRoute('admin.property.index');
     }
 }
 
